8330d506ae2316ede5a8ed528d6acc72e808855b
Jorge Guzman <jorgeguzman@Jorges-MacBook-Air.local>
2024-07-04 15:54:29 -0400

    started server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
+const inquirer = require('inquire');
 const { Pool } = requiere('pg');
+
+const pool = new Pool (
+    {
+        user: 'postgres',
+        password: 'Pa$$word',
+        host: 'localhost',
+        database: 'employees_db'
+    },
+)
+pool.connect();
+
+inquirer
+.prompt([
+    {
+        
+    }
+])
+
